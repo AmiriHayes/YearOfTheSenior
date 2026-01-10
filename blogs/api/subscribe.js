84bcc209d0259ae81a10e69690b07267720fc087
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2026-01-10 17:33:05 -0500

    forgot cors

--- blogs/api/subscribe.js
+++ blogs/api/subscribe.js
@@ -6,6 +6,17 @@ export default async function handler(request, response) {
     return response.status(405).json({ error: 'Method not allowed' });
   }
 
+  // CORS
+  response.setHeader('Access-Control-Allow-Credentials', true)
+  response.setHeader('Access-Control-Allow-Origin', '*') // Allows localhost and other sites
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  response.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+
+  if (request.method === 'OPTIONS') {
+    response.status(200).end()
+    return
+  }
+
   try {
     const { email } = request.body;
 
@@ -16,6 +27,7 @@ export default async function handler(request, response) {
 
     return response.status(200).json({ message: 'Email saved!' });
   } catch (error) {
-    return response.status(500).json({ error: 'Failed to save to database' });
+    // return response.status(500).json({ error: 'Failed to save to database' });
+    return response.status(500).json({ error: err.message })
   }
 }
